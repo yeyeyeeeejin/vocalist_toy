85c102b3f7c6c56f68bf5725c3def013682c874e
yeyeyeeeejin <79955539+yeyeyeeeejin@users.noreply.github.com>
2024-05-13 16:24:29 +0900

    first commit before DB

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import React,{useState,useEffect}  from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { collection, getDocs } from "firebase/firestore";
+import { db } from './config/firebase';
 
 function App() {
+  const[word,setWord] =useState();
+
+  const getWordlist = async () =>{
+    const querySnapshot = await getDocs(collection(db, "vocalist"));
+    querySnapshot.forEach((doc) => {
+    // doc.data() is never undefined for query doc snapshots
+      console.log(doc.id, " => ", doc.data());
+  });
+  setWord(word);
+  }
+  
+  useEffect(() => {
+    getWordlist();
+  },[]);
+  
   return (
     <div className="App">
       <header className="App-header">
